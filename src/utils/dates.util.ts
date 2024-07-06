85b73f174fee1933d7c400483df390eaafb46093
Alveiro Diaz <alveiro.diaz1@gmail.com>
2024-07-05 22:22:29 -0500

    Update dates.util.ts

--- src/utils/dates.util.ts
+++ src/utils/dates.util.ts
@@ -25,10 +25,10 @@ export async function obtenerDiasDeLaSemanaEntreFechas(startDateStr, endDateStr)
 
 export async function obtenerClaveParaFecha(fecha) {
   const diasSemana = ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'];
-  const fechaObj = new Date(fecha + "T00:00:00Z"); // Asegúrate de que la fecha esté en formato ISO 8601 y en UTC
-  const diaSemana = fechaObj.getUTCDay(); // Usa getUTCDay() para obtener el día de la semana en UTC
-  const dia = fechaObj.getUTCDate();
-  const mes = fechaObj.getUTCMonth() + 1; // Los meses en JavaScript son 0-indexados
+  const fechaObj = moment(fecha).tz('America/Bogota'); // Asegúrate de que la fecha esté en formato ISO 8601 y en UTC
+  const diaSemana = fechaObj.day(); // Usa getUTCDay() para obtener el día de la semana en UTC
+  const dia = fechaObj.date();
+  const mes = fechaObj.month() + 1; // Los meses en JavaScript son 0-indexados
 
   return `${diasSemana[diaSemana]} ${dia < 10 ? '0' : ''}${dia}`;
 }
